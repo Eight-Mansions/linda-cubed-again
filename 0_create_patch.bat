6ca0e183799bc4fbd6e0c78b71cdf31c92bb65b8
esperknight <esperknight@yahoo.com>
2022-10-17 21:28:25 -0400

    Fixed poor 1 and 2 getting messed up

--- 0_create_patch.bat
+++ 0_create_patch.bat
@@ -1,2 +1,2 @@
-del linda-psx-patcher\patch\patch.xdelta3
-tools\xdelta3-3.0.11-x86_64.exe -9 -S djw -B 1812725760 -e -vfs cd\orig\linda.bin cd\linda.bin linda-psx-patcher\patch\patch.xdelta3
+del linda-psx-patcher\patch\patch.xdelta
+tools\xdelta3-3.0.11-x86_64.exe -9 -S none -B 1812725760 -e -vfs cd\orig\linda.bin cd\linda.bin linda-psx-patcher\patch\patch.xdelta
