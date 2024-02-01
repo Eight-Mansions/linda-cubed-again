74c79008e668edf1b8dbf528415550f8ee34c1d2
esperknight <esperknight@yahoo.com>
2024-01-31 22:30:34 -0500

    Fixed fruits question... again

--- 0_create_patch.bat
+++ 0_create_patch.bat
@@ -1,2 +1,4 @@
 del linda-psx-patcher\patch_data\patch.xdelta
 tools\xdelta3-3.0.11-x86_64.exe -9 -S none -B 1812725760 -e -vfs cd\linda_original.bin cd\linda_working.bin linda-psx-patcher\patch_data\patch.xdelta
+
+tools\xdelta3-3.0.11-x86_64.exe -9 -S none -B 1812725760 -e -vfs cd\orig_2\linda.bin cd\linda_working.bin linda-psx-patcher\patch_data\patch-other.xdelta
