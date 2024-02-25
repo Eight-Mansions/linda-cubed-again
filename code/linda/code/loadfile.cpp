8f148ae850e671a14167dc0ff425239f1496ac80
esperknight <esperknight@yahoo.com>
2024-02-24 21:19:59 -0500

    Moved code being injected to further in RAM so it won't get clobbered by... something ;_;

--- code/linda/code/loadfile.cpp
+++ code/linda/code/loadfile.cpp
@@ -6,7 +6,7 @@ void LoadSubtitles(u_long* filepos)
 	u_long fileinfo = GetFileInfo(filename);
 	if (fileinfo != 0)
 	{
-		LoadFile(*(int*)(fileinfo + 0x10), 0x801D0000, *(int*)(fileinfo + 0x14));
+		LoadFile(*(int*)(fileinfo + 0x10), 0x801D1000, *(int*)(fileinfo + 0x14));
 	}
 
 	return;
